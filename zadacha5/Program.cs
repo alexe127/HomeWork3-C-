ceb046440c1c76c14b04aca06aec69dc33924ea0
«Alex» <aser_e@mail.ru>
2022-03-27 00:01:41 +0900

    zadacha5

---
+++ zadacha5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+Random random = new Random();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0,2); 
+    Console.Write(array[i] + " ");
+}
