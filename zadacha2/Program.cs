8d02514beca69bf4a35a1ee1d5356fa039d65e53
«Alex» <aser_e@mail.ru>
2022-03-27 14:41:34 +0900

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число N > 0 : ");
+int N = int.Parse(Console.ReadLine());
+
+int fact = 1;
+
+for (int i = 1; i <= N; i++)
+{
+    fact = fact * i;
+}
+Console.Write( "произведение чисел от 1 до " + N + " = " + fact);
