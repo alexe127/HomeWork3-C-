712ef9621358cc34a315d36eddc560b2bcac0982
«Alex» <aser_e@mail.ru>
2022-03-27 15:21:20 +0900

    zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+int num =  int.Parse(Console.ReadLine());
+int i = num;
+int sum = 0;
+while (i > 0)
+{
+    
+    sum = sum + num % 10;
+    
+}
+
+Console.WriteLine(" " + sum);
