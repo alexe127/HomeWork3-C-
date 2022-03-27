0ae35215fb99fb24c843b66536a8caef75f6f63f
«Alex» <aser_e@mail.ru>
2022-03-27 15:22:03 +0900

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите число N > 0 : ");
+int N = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= N; i++)
+{
+   Console.Write( i * i * i + " ");
+}
+
