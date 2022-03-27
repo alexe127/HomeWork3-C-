a027af83cb2dc72d421c9e5fffe45a2cba54fa16
alexe127 <99885886+alexe127@users.noreply.github.com>
2022-03-27 15:36:36 +0900

    Update Program.cs

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -1,12 +1,1 @@
 ﻿// Подсчитать сумму цифр в числе
-int num =  int.Parse(Console.ReadLine());
-int i = num;
-int sum = 0;
-while (i > 0)
-{
-    
-    sum = sum + num % 10;
-    
-}
-
-Console.WriteLine(" " + sum);
