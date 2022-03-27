01dcc1ebdaea86fe491dbd18e4142e9171c06a7e
«Alex» <aser_e@mail.ru>
2022-03-27 14:23:52 +0900

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    sum = sum + i;
+}
+
+Console.WriteLine("Сумма чисел от 1 до А = " + sum);
