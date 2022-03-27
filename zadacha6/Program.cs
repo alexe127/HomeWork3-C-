bcf0dfbb9999cbe904a92ebebbde1d064ae2e1ce
«Alex» <aser_e@mail.ru>
2022-03-27 14:05:16 +0900

    zadacha6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+Random random = new Random();
+int[] array = new int[12];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 10);
+    Console.Write(array[i] + " ");
+    if (array[i] > 0)
+    {
+        sum = sum + array[i];
+    }
+
+}
+
+Console.WriteLine("Сумма положительных элементов = " + sum);
+
+
